819bbc3b0b52bfe2fb6411114697dc20c03e3601
christianbienias <58197019+christianbienias@users.noreply.github.com>
2021-04-27 09:03:35 +0200

    Update run.py

--- app/run.py
+++ app/run.py
@@ -28,11 +28,11 @@ def tokenize(text):
     return clean_tokens
 
 # load data
-engine = create_engine('sqlite:///data/DisasterResponse.db')
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
 df = pd.read_sql_table('messages_disaster', engine)
 
 # load model
-model = joblib.load("/home/workspace/models/classifier.pkl")
+model = joblib.load("../models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
